90dc070dc3024c08636b9fa38016f9e5c959f610
siemeris <siemeris@gmail.com>
2022-06-17 19:35:39 +0000

    uploading my work

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-// esta es mi función que suma dos números
-const sum = (a,b) => {
-    return a + b
+// // esta es mi función que suma dos números
+// const sum = (a,b) => {
+//     return a + b
+// }
+
+// // solo un registro en consola para nosotros.
+// console.log(sum(7,3))
+
+// // exporta la función para usarla en otros archivos 
+// // (similar a la palabra clave `export` cuando se usa webpack)
+// module.exports = { sum };
+
+// one euro is:
+const oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
 }
 
-// solo un registro en consola para nosotros.
-console.log(sum(7,3))
+function fromEuroToDollar(euro){
+    return dollars = euro * oneEuroIs.USD;
+}
 
-// exporta la función para usarla en otros archivos 
-// (similar a la palabra clave `export` cuando se usa webpack)
-module.exports = { sum };
+
+function fromDollarToYen(dollar){
+    //dollar a euro
+    let euro = dollar * (1 / oneEuroIs.USD);
+    let Yen = euro * oneEuroIs.JPY;
+    return Yen.toFixed(2)
+};
+
+function fromYenToPound(Yen){
+    if(typeof Yen == "string"){
+        let msg = "The input is a string"
+        return msg
+    }
+    else{
+    let euro = Yen * (1/oneEuroIs.JPY); 
+    let Pound = euro * oneEuroIs.GBP; 
+    return Pound.toFixed(2) 
+    }
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound};
--- test.js
+++ test.js
@@ -1,11 +1,71 @@
-// importar la función sum del archivo app.js
-const { sum } = require('./app.js');
+// // importar la función sum del archivo app.js
+// const { sum } = require('./app.js');
 
-// comienza tu primera prueba
-test('adds 14 + 9 to equal 23', () => {
-    //dentro de la prueba llamamos a nuestra función sum con 2 números
-    let total = sum(14, 9);
+// // comienza tu primera prueba
+// test('adds 14 + 9 to equal 23', () => {
+//     //dentro de la prueba llamamos a nuestra función sum con 2 números
+//     let total = sum(14, 9);
 
-    // esperamos que la suma de esos 2 números sea 23
-    expect(total).toBe(23);
-});
+//     // esperamos que la suma de esos 2 números sea 23
+//     expect(total).toBe(23);
+// });
+
+test("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const dollars = fromEuroToDollar(3.5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
+    
+    // this is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
+
+test("Five dollars should be 532.92 dollars", function(){
+    //import the function from app.js
+    const { fromDollarToYen } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const yen = fromDollarToYen(5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    let num = 5 / 1.2 * 127.9 ; 
+    const expected = num.toFixed(2); 
+    
+    
+    // this is the comparison for the unit test
+     expect(fromDollarToYen(5)).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
+
+test("Five Yen should be 0,03 Pound", function(){
+    //import the function from app.js
+    const { fromYenToPound } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const pound = fromYenToPound(5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    let num = 5 / 127.9 * 0.8;
+    const expected = num.toFixed(2); 
+    
+    // this is the comparison for the unit test
+     expect(pound).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
+
+test("A string should return 'The input is a string'", function(){
+    //import the function from app.js
+    const { fromYenToPound } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const pound = fromYenToPound('a')
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    let msg = 'The input is a string';
+    const expected = msg; 
+    
+    // this is the comparison for the unit test
+     expect(pound).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
