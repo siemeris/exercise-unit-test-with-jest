c616b756125204ca07bea11f20bcab59cb2ec856
siemeris <siemeris@gmail.com>
2022-06-19 16:57:12 +0000

    uploading test str

--- test.js
+++ test.js
@@ -21,7 +21,7 @@ test("One euro should be 1.206 dollars", function(){
     const expected = 3.5 * 1.2; 
     
     // this is the comparison for the unit test
-     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(dollars).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
 
 test("Five dollars should be 532.92 dollars", function(){
@@ -56,16 +56,11 @@ test("Five Yen should be 0,03 Pound", function(){
 })
 
 test("A string should return 'The input is a string'", function(){
-    //import the function from app.js
+
     const { fromYenToPound } = require('./app.js')
-
-    // use the function like its suppoed to be used
     const pound = fromYenToPound('a')
-
-    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
     let msg = 'The input is a string';
     const expected = msg; 
-    
     // this is the comparison for the unit test
      expect(pound).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
